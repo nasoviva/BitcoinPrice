07d60dee579662b4655547f04217f6b4a31767b5
nasoviva <nasoviva@gmail.com>
2024-06-23 22:21:46 -0300

    mvc

---
+++ BitcoinPrice/Controller/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  BitcoinPrice
+//
+//  Created by Victoria on 23/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+
+    var coinManager = CoinManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        coinManager.delegate = self
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+
+}
+
+extension ViewController: UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+}
+
+extension ViewController: CoinManagerDelegate {
+
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+
+    func didFailWithError(error: any Error) {
+        print(error)
+    }
+}
+
