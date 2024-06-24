3e9cddf2ca6f6caed8bf7b239f6f7f1ba36c48c0
nasoviva <nasoviva@gmail.com>
2024-06-23 22:20:07 -0300

    add functionality

---
+++ BitcoinPrice/Model/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  CoinData.swift
+//  BitcoinPrice
+//
+//  Created by Victoria on 23/06/2024.
+//
+
+import Foundation
---
+++ BitcoinPrice/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  BitcoinPrice
+//
+//  Created by Victoria on 23/06/2024.
+//
+
+import Foundation
