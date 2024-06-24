385651d5adeb8c6a40a59a1c0b9426a3d1398ba0
Victoria <98150826+nasoviva@users.noreply.github.com>
2024-06-24 10:30:40 -0300

    Update CoinManager.swift

--- BitcoinPrice/Model/CoinManager.swift
+++ BitcoinPrice/Model/CoinManager.swift
@@ -15,7 +15,7 @@ protocol CoinManagerDelegate {
 struct CoinManager {
 
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "06B5BAC3-4A99-4EC2-9CEE-89A402D1BB93"
+    let apiKey = "***"
 
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
